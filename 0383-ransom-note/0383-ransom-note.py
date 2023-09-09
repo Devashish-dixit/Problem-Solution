cdcc90ce161c37ddeeef4e8bb31c299a9b685b1a
Devashish Dixit <109742026+Devashish-dixit@users.noreply.github.com>
2023-09-09 17:41:42 +0530

    Time: 49 ms (56.01%), Space: 13.6 MB (21.2%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        dic ={}
+        for char in magazine:
+            if char not in dic:
+                dic[char] = 1
+            else:
+                dic[char] +=1
+        for char in ransomNote:
+            if char in dic and dic[char]>0:
+                dic[char] -= 1
+            else:
+                return False
+        return True
